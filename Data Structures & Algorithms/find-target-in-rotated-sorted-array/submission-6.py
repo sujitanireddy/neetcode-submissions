b5f237647afef9b3d653c80fa072095308e7b2a1
Sujit <anireddysujitreddy@gmail.com>
2026-04-06 06:08:33 -0500

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.py
@@ -0,0 +1,65 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        #Algorithm
+        #Binary search for the min in rotated sorted array.
+        #Slice the array into two sub arrays and binary search them.
+
+        def binary_search(arr):
+            
+            L = 0 
+            R = len(arr) - 1
+
+            while L <= R:
+
+                mid = (L + R) // 2
+
+                if arr[mid] == target:
+                    return mid
+
+                elif arr[mid] > target:
+                    R = mid - 1
+
+                else:
+                    L = mid + 1
+
+            return -1 
+
+
+        L = 0
+        R = len(nums) - 1
+
+        while L < R:
+
+            mid = (L + R) // 2
+
+            if nums[R] < nums[mid]:
+
+                L = mid + 1
+            
+            else:
+
+                R = mid
+        
+        partition_point = R
+
+        left_partition = nums[:partition_point] 
+        right_partition = nums[partition_point:]
+
+        print(left_partition)
+        print(right_partition)
+
+        if binary_search(left_partition) != -1:
+            return binary_search(left_partition)
+        
+        elif binary_search(right_partition) != -1:
+            return len(left_partition) + binary_search(right_partition)
+        
+        else:
+            return -1 
+
+
+        
+        
+        
+
