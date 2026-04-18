c5d65ccfa06a23beca6c5829ce8045ccdb17f460
Sujit <anireddysujitreddy@gmail.com>
2026-04-18 08:01:56 -0500

    Add: find-target-in-rotated-sorted-array - submission-23

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-23.py
@@ -0,0 +1,48 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        #find the min in rotated sorted array
+        #Assign L and R pointers based on where target lies in the array
+        #Binary Search
+
+        L = 0 
+        R = len(nums) - 1 
+
+        while L < R:
+
+            mid = (L+R) // 2 
+
+            if nums[mid] > nums[R]:
+                L = mid + 1 
+
+            else:
+                R = mid
+        
+        pivot = L
+
+        if nums[0] <= target <= nums[-1]:
+            L = 0 
+            R = len(nums) - 1
+
+        elif nums[0] <= target <= nums[pivot - 1]:
+            L = 0
+            R = pivot - 1 
+        
+        else:
+            L = pivot
+            R = len(nums) - 1
+        
+        while L <= R:
+
+            mid = (L+R) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] > target:
+                R = mid - 1 
+
+            else:
+                L = mid + 1 
+        
+        return - 1
