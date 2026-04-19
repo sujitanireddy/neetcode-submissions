4548e1f7e0124059ca74114f25dc25758d162fa6
Sujit <anireddysujitreddy@gmail.com>
2026-04-19 08:58:04 -0500

    Add: asteroid-collision - submission-11

---
+++ Data Structures & Algorithms/asteroid-collision/submission-11.py
@@ -0,0 +1,24 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stk = []
+
+        for a in asteroids:
+
+            while stk and a < 0 and stk[-1] > 0:
+
+                if stk[-1] == abs(a):
+                    stk.pop()
+                    a = 0
+                
+                elif stk[-1] < abs(a):
+                    stk.pop()
+                
+                else:
+                    a = 0
+
+            if a != 0:
+                stk.append(a)
+        
+        return stk
+            
