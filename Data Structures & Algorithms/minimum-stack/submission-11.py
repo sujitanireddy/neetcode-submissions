9bd5788d1c3add0deadfc548c50c795a28193597
Sujit <anireddysujitreddy@gmail.com>
2026-05-29 14:59:06 -0500

    Add: minimum-stack - submission-11

---
+++ Data Structures & Algorithms/minimum-stack/submission-11.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.minstk = []
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        if self.minstk: 
+            val = min(self.minstk[-1], val)
+        self.minstk.append(val)
+
+    def pop(self) -> None:
+        del self.stk[-1]
+        del self.minstk[-1]
+        
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        return self.minstk[-1]
