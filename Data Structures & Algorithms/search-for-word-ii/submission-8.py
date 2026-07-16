78832cf6eb190ead0846d751c64a161685d33ee3
Sujit <anireddysujitreddy@gmail.com>
2026-07-16 15:02:05 -0500

    Add: search-for-word-ii - submission-8

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-8.py
@@ -0,0 +1,62 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+    
+    def insert(self, word):
+        curr = self
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        root = TrieNode()
+        ROWS = len(board)
+        COLS = len(board[0])
+        visited = set()
+        res = set()
+
+        for word in words:
+            root.insert(word)
+
+        def dfs(r, c, node, word):
+
+                if r < 0 or c < 0 or r == ROWS or c == COLS or (r,c) in visited or board[r][c] not in node.children:
+                    return 
+                
+                visited.add((r,c))
+                node = node.children[board[r][c]]
+                word += board[r][c]
+                if node.word:
+                    res.add(word)
+
+                dfs(r+1,c,node,word)
+                dfs(r-1,c,node,word)
+                dfs(r,c+1,node,word)
+                dfs(r,c-1,node,word)
+
+                visited.remove((r,c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c,root,"")
+
+        return list(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
