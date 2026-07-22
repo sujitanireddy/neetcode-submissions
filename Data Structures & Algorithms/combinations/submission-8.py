4ae2847ad7a8085cd2b5c8b35b30ff8935558d62
Sujit <anireddysujitreddy@gmail.com>
2026-07-22 08:04:34 -0500

    Add: combinations - submission-8

---
+++ Data Structures & Algorithms/combinations/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        res = []
+        sol = []
+
+        def backtrack(i):
+
+            if len(sol) == k:
+                res.append(sol[:])
+                return
+            
+            if i > n:
+                return
+
+            sol.append(i)
+            backtrack(i+1)
+            sol.pop()
+
+            backtrack(i+1)
+
+        backtrack(1)
+
+        return res
