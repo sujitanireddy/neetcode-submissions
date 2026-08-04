a6ffd1cdbe58792b49503b76865e1996fe252c6a
Sujit <anireddysujitreddy@gmail.com>
2026-08-04 06:18:58 -0500

    Add: valid-tree - submission-10

---
+++ Data Structures & Algorithms/valid-tree/submission-10.py
@@ -0,0 +1,37 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        """
+        Valid Tree: No cylces, all nodes are connected
+
+        E = (V - 1) (use this to return early). If all nodes are connected then it's a tree
+
+        {
+            0 : [1]
+            1 : [0,2,3,4]
+            2 : [1,3]
+            3 : [2,1]
+            4 : [1]
+        }
+
+        """
+        if len(edges) >= n:
+            return False
+
+        visit = set()
+        adjList = defaultdict(list)
+        for u, v in edges:
+            adjList[u].append(v)
+            adjList[v].append(u)
+        
+        def dfs(n):
+            
+            visit.add(n)
+
+            for nei in adjList[n]:
+                if nei not in visit:
+                    dfs(nei)
+
+        dfs(0)
+
+        return len(visit) == n
