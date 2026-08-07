3bcf8ff54f1382bc715c7c9fe5114902a65a4b30
Sujit <anireddysujitreddy@gmail.com>
2026-08-07 05:31:12 -0500

    Add: two-integer-sum - submission-31

---
+++ Data Structures & Algorithms/two-integer-sum/submission-31.py
@@ -0,0 +1,30 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        """
+        hashamp = {4:0}
+
+            {
+                6 : 0
+                5 : 1
+
+            }
+
+
+
+        """
+        hashmap = defaultdict(int)
+
+        for i in range(len(nums)):
+
+            if (target - nums[i]) in hashmap:
+
+                return [hashmap[target - nums[i]], i]
+            
+            hashmap[nums[i]] = i
+
+
+
+            
+
+            
