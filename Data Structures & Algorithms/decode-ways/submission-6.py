2ccd7bb9817e1150dcf564bbaf8e16f49998ede9
Sujit <anireddysujitreddy@gmail.com>
2026-08-30 12:46:02 -0500

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        n = len(s)
+        self.count = 0
+        cache = {} 
+        
+        def recurse(i):
+
+            if i in cache:
+                return cache[i]
+
+            if i == n:
+                return 1
+
+            if s[i] == "0":
+                return 0
+            
+            cache[i] = recurse(i+1)
+
+            #two digit check
+            if i < (n - 1) and 10 <= int(s[i:i+2]) <= 26:
+                cache[i] += recurse(i+2)    
+
+            return cache[i]
+        
+        return recurse(0)
