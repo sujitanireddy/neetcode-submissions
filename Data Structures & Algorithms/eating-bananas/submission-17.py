bbb51187c2c5309f7172226095c4942678852d64
Sujit <anireddysujitreddy@gmail.com>
2026-09-03 10:34:50 -0500

    Add: eating-bananas - submission-17

---
+++ Data Structures & Algorithms/eating-bananas/submission-17.py
@@ -0,0 +1,46 @@
+"""
+, h = 9
+   
+     M
+     L 
+     R 
+   
+ 1 2 1 1
+[1,4,3,2]
+
+
+L M   R
+1 2 3 4
+
+
+
+if k works: mid = R
+else L = mid + 1
+
+"""
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        def valid_k(k):
+            
+            time_taken = 0
+
+            for pile in piles:
+                time_taken += math.ceil(pile/k)
+
+            return time_taken <= h
+
+        L = 1
+        R = max(piles)
+
+        while L < R:
+
+            mid = (L+R) // 2
+
+            if valid_k(mid):
+                R = mid
+            
+            else:
+                L = mid + 1
+        
+        return L
