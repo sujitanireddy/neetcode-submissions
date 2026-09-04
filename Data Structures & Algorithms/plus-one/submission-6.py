3f85c97510375ccad573ce5d5c9630a5832f30fe
Sujit <anireddysujitreddy@gmail.com>
2026-09-04 07:32:39 -0500

    Add: plus-one - submission-6

---
+++ Data Structures & Algorithms/plus-one/submission-6.py
@@ -0,0 +1,24 @@
+"""
+TC: O(n)
+SC: O(n)
+
+[9,9,9]
+
+int ("999") = 999 + 1 = 1000
+[ 1, 0, 0 , 0]
+
+"""
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        for i in range(len(digits) - 1,-1,-1):
+            
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            else:
+                digits[i] = 0
+        
+        return [1] + digits
