aa74173886e45b70ad5a2f3d5ffb70faab9b4202
Sujit <anireddysujitreddy@gmail.com>
2026-09-15 13:46:49 -0500

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        cache = {}
+
+        target = len(nums) - 1
+        
+        def dfs(i):
+
+            if i in cache:
+                return cache[i]
+            
+            if i == target:
+                return True
+            
+            if i > target:
+                return False
+            
+            for j in range(1, (nums[i] + 1)):
+                if dfs(i+j):
+                    cache[i] = True
+                    return True
+
+            cache[i] = False
+            
+            return cache[i]
+
+        return dfs(0)
+
