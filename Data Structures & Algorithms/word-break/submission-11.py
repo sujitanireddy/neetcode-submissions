70b8f28cc3c6333ca8d2d1cc8f9c6b0171fc8911
Sujit <anireddysujitreddy@gmail.com>
2026-09-22 03:33:07 -0700

    Add: word-break - submission-11

---
+++ Data Structures & Algorithms/word-break/submission-11.py
@@ -0,0 +1,35 @@
+"""
+0 1 2 3 4 5 6 7 8
+n e e t c o d e
+wordDict = set()
+
+BruteForce : O(2**n)
+
+n   e   e   t   c   o   d   e
+
+0   1   2   3   4   5   6   7   8
+
+T   F   F   F   T   F   F   F   T
+
+
+
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+
+            for w in wordDict:
+
+                if (i + len(w)) <= len(s) and w == s[i: i + len(w)]:
+
+                    dp[i] = dp[i + len(w)]
+                
+                if dp[i]:
+                    break
+        
+        return dp[0]
+
